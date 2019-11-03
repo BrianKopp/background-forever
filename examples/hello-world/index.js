1ccf50c33b6bba221dfb83e7d9e4b1001f1c6b94
Brian Kopp <briankopp.usa@gmail.com>
2019-11-03 09:52:37 -0700

    Add examples (#1)
    * Add basic js example
    
    * Update with stop message
    
    * Update examples for js and typescript

---
+++ examples/hello-world/index.js
@@ -0,0 +1,53 @@
+const BackgroundForever = new require('background-forever').BackgroundForever;
+const bf = new BackgroundForever(() => {
+    return new Promise((resolve, reject) => {
+        console.log('hello world!');
+        setTimeout(() => {
+            resolve();
+        }, 1000);
+    });
+});
+
+bf.on('beforeRun', (runCount) => {
+    console.log('preparing to start run #' + runCount);
+});
+
+bf.on('runSuccess', () => {
+    console.log('finished my function');
+});
+
+bf.on('runError', (e) => {
+    console.error('should not hit this event with my simple hello world function', e);
+});
+
+bf.start();
+
+setTimeout(() => {
+    bf.stop().then(() => {
+        console.log('successfully stopped my function!');
+        process.exit(0);
+    }).catch((e) => {
+        console.error('failed to stop my function', e);
+        process.exit(1);
+    });
+}, 5000);
+
+/*
+Outputs:
+preparing to start run #1
+hello world!
+finished my function
+preparing to start run #2
+hello world!
+finished my function
+preparing to start run #3
+hello world!
+finished my function
+preparing to start run #4
+hello world!
+finished my function
+preparing to start run #5
+hello world!
+finished my function
+successfully stopped my function!
+*/
